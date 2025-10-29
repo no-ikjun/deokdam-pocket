73c4da943159ecd741eb62c13287396cbab4a48b
ikjunchoi <choiikjun1101@gmail.com>
2025-10-29 17:12:00 +0900

    feat: implement PATCH endpoint for updating pocket details with authorization checks

--- src/app/api/pocket/route.ts
+++ src/app/api/pocket/route.ts
@@ -92,6 +92,79 @@ export async function GET(req: Request) {
   }
 }
 
+export async function PATCH(req: Request) {
+  const client = await db.connect();
+  const token = cookies().get("token")?.value;
+  if (!token) {
+    client.release();
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET!);
+    const { user_uuid } = payload as {
+      user_uuid: string;
+    };
+    if (!user_uuid) {
+      client.release();
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const request = await req.json();
+    const { pocket_uuid, name, icon, maxMembers, goalCount, openDate } =
+      request as {
+        pocket_uuid: string;
+        name: string;
+        icon: string;
+        maxMembers: number;
+        goalCount: number;
+        openDate: string;
+      };
+
+    if (
+      !pocket_uuid ||
+      !name ||
+      !icon ||
+      !maxMembers ||
+      !goalCount ||
+      !openDate
+    ) {
+      client.release();
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 }
+      );
+    }
+
+    const pocketCheck =
+      await client.sql`SELECT made_by FROM pocket WHERE pocket_uuid = ${pocket_uuid}`;
+    if (pocketCheck.rows.length === 0) {
+      client.release();
+      return NextResponse.json(
+        { message: "Pocket not found" },
+        { status: 404 }
+      );
+    }
+    if (pocketCheck.rows[0].made_by !== user_uuid) {
+      client.release();
+      return NextResponse.json(
+        { message: "Only the creator can update the pocket" },
+        { status: 403 }
+      );
+    }
+
+    const openAt = new Date(openDate);
+    const openAtIso = openAt.toISOString();
+
+    await client.sql`UPDATE pocket SET name = ${name}, icon = ${icon}, "limit" = ${maxMembers}, goal = ${goalCount}, open_at = ${openAtIso} WHERE pocket_uuid = ${pocket_uuid};`;
+
+    client.release();
+    return NextResponse.json({ message: "success" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    client.release();
+    return NextResponse.json({ message: "error" }, { status: 500 });
+  }
+}
+
 export async function DELETE(req: Request) {
   const client = await db.connect();
   const token = cookies().get("token")?.value;
